0a75696d0024758976ee5d01ad172c3243d0cfd2
Gotphoenix3 <mikemartinezch15@gmail.com>
2024-09-05 12:39:53 -0600

    Updating the generate password  function

--- main.py
+++ main.py
@@ -8,6 +8,43 @@ font = ("Arial", 12)
 data_file_path = "password_manager.txt"
 
 
+# ---------------------------- SEARCH FUNCTION ------------------------------------#
+def find_password():
+    """
+    This function allows the user to search of r a website in the json file
+    """
+    # Variable to save the website the user enter to search
+    lookup_website = website_entry.get()
+    # Error message when the input is empty
+    if lookup_website == "":
+        messagebox.showerror(title="Error", message="Can not search for a Null string.")
+    else:
+        try:
+            with open("data.json", "r") as data_file:
+                try:
+                    website_information = json.load(data_file)[lookup_website]
+                    website_message = (f"Website information:\nEmail: {website_information["email"]}\nPassword: "
+                                       f"{website_information["password"]}")
+                    messagebox.showinfo(title=f"{lookup_website.title()} data.", message=website_message)
+                except KeyError:
+                    messagebox.showerror(title="Error: 404 Key Error", message=f"{lookup_website} has no information.")
+        # Handling error in case the file json is not created or found
+        except FileNotFoundError:
+            # Error message: file not found
+            file_not_found_answer = messagebox.askokcancel(title="Error: File not found", message=f"The "
+                                                                                                  f"file you are "
+                                                                                                  f"looking for is not "
+                                                                                                  f"created.\nDo you "
+                                                                                                  f"want to create it "
+                                                                                                  f"(Ok)?")
+            if file_not_found_answer:
+                with open("data.json", "w") as _:
+                    pass
+        # Handling error in case the file has no entries
+        except ValueError:
+            messagebox.showerror(title=f"Error: Value Error", message="File data.json has no entries")
+
+
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 def generate_password():
     """
@@ -52,6 +89,7 @@ def get_data_add():
             "password": user_password,
         }
     }
+
     # We validate the user input, if there is empty spaces then pop up an error message
     if user_website == "" or user_email == "" or user_password == "":
         messagebox.showerror(title="Error", message="Please fill all the camps :c")
@@ -62,22 +100,28 @@ def get_data_add():
         #                                                                   f"Password: {user_password} \n"
         #                                                                   f"Is it okay to save?")
         # if valid_answer:
-        with open("data.json", "r") as file:
-            # Reading old data
-            data_json = json.load(file)
-            # Updating the old data with new data
-            data_json.update(new_data)
-        with open("data.json", "w") as file:
-            # Saving updating data
-            json.dump(data_json, file, indent=4)
+        try:
+            with open("data.json", "r") as data_file:
+                # Json dump is used to write a new json entry in a file
+                # json.dump(new_data, data_file, indent=4)
 
+                # Json load is used to read json from a file
+                # data = json.load(data_file)
+                # print(data)
+
+                # json update is used to update a json
+                data = json.load(data_file)  # Reading the old data
+        except FileNotFoundError:
+            with open("data.json", "w") as data_file:
+                json.dump(new_data, data_file, indent=4)
+        else:
+            data.update(new_data)  # Updating the old data with the new data
+
+            with open("data.json", "w") as data_file:
+                json.dump(data, data_file, indent=4)  # Saving the updated data
+        finally:
             website_entry.delete(0, END)
             password_entry.delete(0, END)
-            # This was the original formatting of the data.txt file
-            # file.write(f"{user_website.title()} data = {{\n")
-            # file.write(f"   Email/User: {user_email},\n")
-            # file.write(f"   Password: {user_password},\n")
-            # file.write("}\n")
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -98,7 +142,7 @@ email_label = Label(window, text=" Email/Username:", font=font)
 password_label = Label(window, text="            Password: ", font=font)
 
 # Now we create the user entry labels.
-website_entry = Entry(window, font=font, width=40)
+website_entry = Entry(window, font=font, width=21)
 # We call the focus function to center the cursor in the entry of the website
 website_entry.focus()
 email_entry = Entry(window, font=font, width=40)
@@ -110,16 +154,20 @@ password_entry = Entry(window, font=font, width=21)
 generate_button = Button(window, text="Generate password", font=font, command=generate_password)
 add_button = Button(window, text="Add", font=font, width=39, command=get_data_add)
 
+# Creating the search button
+search_button = Button(window, text="Search", font=font, width=16, command=find_password)
+
 # Assign the widgets to the screen using grid
 var_pad_x = 5
 var_pad_y = 5
 website_label.grid(column=0, row=1)
 email_label.grid(column=0, row=2)
 password_label.grid(column=0, row=3)
-website_entry.grid(column=1, row=1, columnspan=2, padx=var_pad_x, pady=var_pad_y)
+website_entry.grid(column=1, row=1, padx=var_pad_x, pady=var_pad_y)
 email_entry.grid(column=1, row=2, columnspan=2, padx=var_pad_x, pady=var_pad_y)
 password_entry.grid(column=1, row=3, padx=var_pad_x, pady=var_pad_y)
 generate_button.grid(column=2, row=3, padx=var_pad_x, pady=var_pad_y)
 add_button.grid(column=1, row=4, columnspan=2, padx=var_pad_x, pady=var_pad_y)
+search_button.grid(column=2, row=1)
 
 window.mainloop()
